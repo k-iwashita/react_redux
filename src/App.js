52628b402393babc6d86285e86fad07cc1713068
k-iwashita <k.iwashita.gude@gmail.com>
2020-04-11 18:59:43 +0900

    jsxとは

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return(
-      <div>
-        <h1>Hello, World!</h1>
-      </div>
+      <h1>
+        Hello, World!
+      </h1>
     )
   }
 }
